1a0ed35198222f5647aea14af8ac6a42f07cbb11
Izabely Furtado <izabely_furtado@hotmail.com>
2023-08-10 14:59:20 -0300

    configurando para receber json

---
+++ src/mensagem/MensagemErro.java
@@ -0,0 +1,17 @@
+package mensagem;
+
+public class MensagemErro {
+	private String erro;
+
+    public MensagemErro(String erro) {
+        this.erro = erro;
+    }
+
+    public String getErro() {
+        return erro;
+    }
+
+    public void setErro(String erro) {
+        this.erro = erro;
+    }
+}
